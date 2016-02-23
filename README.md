5a08c100a12d4293fe5b9c382cc891e55fd28ef4
Son Truong <sont85@gmail.com>
2016-02-22 17:45:56 -0800

    update style changes read me file"

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 frontend-nanodegree-arcade-game
 ===============================
 
-Students should use this [rubric](https://www.udacity.com/course/viewer/#!/c-nd001/l-2696458597/m-2687128535) for self-checking their submission.
+Star Chaser Arcade Game
 
-For detailed instructions on how to get started, check out this [guide](https://docs.google.com/document/d/1v01aScPjSWCCWQLIpFqvg3-vXLH2e8_SZQKC8jNO0Dc/pub?embedded=true).
+  -Click to start the game.
+  -player can move by pressing the arrow key.
+  -Collect 5 Stars to win the game.
+  -Avoid Colliding with Bugs.
+  -You have 3 lives.
+  -Press enter whenever to restart game.
--- js/app.js
+++ js/app.js
@@ -1,10 +1,45 @@
+'use strict';
+
+// Star Class
+var Star = function(x, y) {
+  this.sprite = 'images/Star.png';
+  this.x = x || Math.floor(Math.random() * 5) * 85;
+  this.y = y || Math.floor(Math.random() * 5) * 85;
+};
+
+// check if player collected the star and reposition it
+Star.prototype.checkCollect = function() {
+  var xdiff = (player.x - this.x);
+  var ydiff = (player.y - this.y);
+
+  if (xdiff >= -50 && xdiff <= 50 && ydiff >= -50 && ydiff <= 30) {
+    player.gainPoint();
+    star.reposition();
+  }
+};
+
+// Randomly reposition star
+Star.prototype.reposition = function() {
+  this.x = Math.floor(Math.random() * 5) * 85;
+  this.y = Math.floor(Math.random() * 5) * 85;
+};
+
+Star.prototype.update = function() {
+  this.checkCollect();
+};
+
+Star.prototype.render = function() {
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+
 // Enemy Class
 var Enemy = function(x, y) {
-    this.sprite = 'images/enemy-bug.png';
-    this.x = x;
-    this.y = y;
-    // enemy speed randomly generated
-    this.speed = Math.floor(Math.random() * 200) + 50;
+  this.sprite = 'images/enemy-bug.png';
+  this.x = x;
+  this.y = y;
+  // enemy speed randomly generated
+  this.speed = Math.floor(Math.random() * 200) + 50;
 };
 
 // update move and check check collisions
@@ -15,23 +50,23 @@ Enemy.prototype.update = function(dt) {
 
 // Enemy moves horizontally
 Enemy.prototype.move = function(dt) {
-    this.x += dt * this.speed;
-}
+  this.x += dt * this.speed;
+};
 
 // Draw the enemy on the screen
 Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 // Check collission between 100px horizontally and 80px vertically of enemy sprite
 Enemy.prototype.checkCollisions = function() {
-    var xdiff = (player.x - this.x);
-    var ydiff = (player.y - this.y);
+  var xdiff = (player.x - this.x);
+  var ydiff = (player.y - this.y);
 
-    if (xdiff >= -50 && xdiff <= 50 && ydiff >= -50 && ydiff <= 30) {
-      player.loseLife()
-    }
-}
+  if (xdiff >= -50 && xdiff <= 50 && ydiff >= -50 && ydiff <= 30) {
+    player.loseLife();
+  }
+};
 
 // Eliminate all Enemies
 Enemy.reset = function() {
@@ -39,7 +74,7 @@ Enemy.reset = function() {
   for (var i = 1; i <= 9999; i++) {
     window.clearInterval(i);
   }
-}
+};
 
 // Instantiate Enemy and push to allEnemies
 Enemy.spawn = function() {
@@ -52,10 +87,10 @@ Enemy.spawn = function() {
 
     // randomized appearance of enemies position
     var bugPositions = [65, 150, 235];
-    var yCoor = bugPositions[Math.floor(Math.random() * 5)]
+    var yCoor = bugPositions[Math.floor(Math.random() * 5)];
     allEnemies.push(new Enemy(0, yCoor));
-  }, Math.random() * 2500) + .5;
-}
+  }, Math.random() * 2500 + 0.5);
+};
 
 
 var Player = function() {
@@ -83,8 +118,8 @@ Player.prototype.boundary = function() {
 
 // Reposition player to starting point when lose life
 Player.prototype.loseLife = function() {
-  this.life--
-  this.x = 200;
+  this.life--;
+    this.x = 200;
   this.y = 400;
 };
 
@@ -113,50 +148,50 @@ Player.prototype.move = function(x, y) {
 // Render Star Collected and Life Info
 Player.prototype.renderScoreBoard = function() {
   ctx.clearRect(0, 0, 500, 45);
-  ctx.font = "48px serif";
-  ctx.fillStyle = "#BD5643"
-  ctx.fillText("Life: " + player.life, 350, 40);
+  ctx.font = '48px serif';
+  ctx.fillStyle = '#BD5643';
+  ctx.fillText('Life: ' + player.life, 350, 40);
 
-  ctx.fillStyle = "#355796"
-  ctx.fillText("Star: " + player.point, 10, 40);
-}
+  ctx.fillStyle = '#355796';
+  ctx.fillText('Star: ' + player.point, 10, 40);
+};
 
 Player.prototype.endGame = function(result) {
   // End Game Text
   ctx.fillText(result, 120, 175);
-  ctx.font = "26px serif";
-  ctx.fillText("Press Enter To Play Again", 125, 350);
+  ctx.font = '26px serif';
+  ctx.fillText('Press Enter To Play Again', 125, 350);
 
   // Remove enemies
   Enemy.reset();
-}
+};
 
 // Render player sprite, scoreboard, and end game status
 Player.prototype.render = function() {
   if (player.win) {
-    this.endGame("Congratulation")
+    this.endGame('Congratulation');
   } else if (player.life <= 0) {
-    this.endGame("Game Over");
+    this.endGame('Game Over');
   }
   this.renderScoreBoard();
   ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 Player.prototype.handleInput = function(dir) {
-  switch(dir) {
-    case "right":
+  switch (dir) {
+    case 'right':
       this.move(10, 0);
       break;
-    case "left":
+    case 'left':
       this.move(-10, 0);
       break;
-    case "up":
+    case 'up':
       this.move(0, -10);
       break;
-    case "down":
+    case 'down':
       this.move(0, 10);
       break;
-    case "new game":
+    case 'new game':
       player = new Player();
       star = new Star();
       Enemy.reset();
@@ -165,56 +200,23 @@ Player.prototype.handleInput = function(dir) {
   }
 };
 
-// Star Class
-var Star = function(x, y) {
-  this.sprite = "images/Star.png"
-  this.x = x || Math.floor(Math.random() * 5) * 85;
-  this.y = y || Math.floor(Math.random() * 5) * 85;
-}
-
-// check if player collected the star and reposition it
-Star.prototype.checkCollect = function() {
-  var xdiff = (player.x - this.x);
-  var ydiff = (player.y - this.y);
-
-  if (xdiff >= -50 && xdiff <= 50 && ydiff >= -50 && ydiff <= 30) {
-    player.gainPoint()
-    star.reposition();
-  }
-}
-
-// Randomly reposition star
-Star.prototype.reposition = function() {
-  this.x = Math.floor(Math.random() * 5) * 85;
-  this.y = Math.floor(Math.random() * 5) * 85;
-}
-
-Star.prototype.update =function() {
-  this.checkCollect();
-}
-
-Star.prototype.render = function() {
-  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-}
-
 // Instantiating star and player
 var star = new Star(200, 300);
 var player = new Player();
-
-// clearing all intervals and enemies then spawn.
 var allEnemies = [];
+// clearing all intervals and enemies then spawn.
 Enemy.reset();
 Enemy.spawn();
 
 // Enter and Arrow Keys are only allow
 document.addEventListener('keydown', function(e) {
-    var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down',
-        13: 'new game'
-    };
+  var allowedKeys = {
+    37: 'left',
+    38: 'up',
+    39: 'right',
+    40: 'down',
+    13: 'new game'
+  };
 
-    player.handleInput(allowedKeys[e.keyCode]);
+  player.handleInput(allowedKeys[e.keyCode]);
 });
